2c6c52b1994507fb321c96b76c6c84dd99208d47
Paul de Wouters <paul@dell>
2012-08-10 18:15:24 -0300

    initial commit

---
+++ wp-contributors.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: WP Contributors
+Plugin URI: http://wpconsult.net/wp-contributors
+Description: display more than one author-name on a post.
+Version: 0.1
+Author: Paul de Wouters
+Author Email: pauldewouters@gmail.com
+License:
+
+  Copyright 2011 Paul de Wouters (pauldewouters@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+
+  
